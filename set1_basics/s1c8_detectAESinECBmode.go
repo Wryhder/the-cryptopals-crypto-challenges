9b8570335786bfd2ec9ef021e05f4f8167f6c68d
Wryhder <jemimahsummers@yahoo.com>
2022-12-20 21:00:47 +0100

    Make function for detecting AES in ECB mode reusable across packages

--- set1_basics/s1c8_detectAESinECBmode.go
+++ set1_basics/s1c8_detectAESinECBmode.go
@@ -4,76 +4,72 @@ package set1_basics
 
 import (
 	"bufio"
-    "fmt"
-    "os"
+	"os"
 	"bytes"
+	"strconv"
+
+	utils "wryhder/cryptopals-crypto-challenges/utilities"
 )
 
-// Break ciphertext into n-byte blocks (assuming padding is not necessary)
-func breakLineIntoBlocks(ciphertext []byte, keySize int) [][]byte {
-	lengthOfCiphertext := len(ciphertext)
-	var cipherTextBlocks [][]byte
-
-	for i := 0; i < lengthOfCiphertext; i+=keySize {
-		cipherTextBlocks = append(cipherTextBlocks, ciphertext[i:i + keySize])
-	}
-
-	return cipherTextBlocks
-}
-
-func DetectAES128_ECB(filePath string) (int, string) {
+func DetectAES128_ECB_File(filePath string) string {
 	f, _ := os.Open(filePath)
 	defer f.Close()
 
-	var repeatedBlocks = make(map[int]int)
 	var allResults = make(map[int]map[string]string)
 	count := 0
-	highestCount := 0
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		line := scanner.Text()
-
-		// for each line, split text into blocks of 16 bytes
-		cipherTextBlocks := breakLineIntoBlocks(HexToByte(line), 16)
-
-		for index, byteBlock := range cipherTextBlocks {
-			for n := 0; n < len(cipherTextBlocks); n++ {
-
-				// Don't compare a block with itself
-				if index == n {
-					continue
-				}
-
-				if bytes.Compare(byteBlock, cipherTextBlocks[n]) == 0 {
-					repeatedBlocks[count] += 1
-				}
-			}
-		}
+		isECB := DetectAES128_ECB(line)
 
 		allResults[count] = map[string]string{
-			"noOfRepeats": fmt.Sprintf("%v", repeatedBlocks[count]),
+			"isECB": strconv.FormatBool(isECB),
 			"ciphertext": line,
-		}
-
-		currentCount := repeatedBlocks[count]
-		if currentCount < highestCount {
-			continue
-		} else {
-			highestCount = currentCount
 		}
 
 		count += 1
 	}
 
-	// Find line with the most number of repeated strings
-	var result string
+	// Find line encrypted in ECB mode
+	var ECBEncryted string
+
 	for _, lineDetails := range allResults {
-		if lineDetails["noOfRepeats"] == fmt.Sprintf("%v", highestCount) {
-			result = lineDetails["ciphertext"]
+		mode, _ := strconv.ParseBool(lineDetails["isECB"])
+		if mode {
+			ECBEncryted = lineDetails["ciphertext"]
 			break
 		}
 	}
+
+	return ECBEncryted
+}
+
+func DetectAES128_ECB(ciphertext string) (bool) {
+	var repeatedBlocks = make(map[int]int)
+
+	cipherTextBlocks := utils.ChunkifyText(HexToByte(ciphertext), 16)
+	for index, byteBlock := range cipherTextBlocks {
+		for n := 0; n < len(cipherTextBlocks); n++ {
+
+			// Don't compare a block with itself
+			if index == n {
+				continue
+			}
+
+			if bytes.Compare(byteBlock, cipherTextBlocks[n]) == 0 {
+				repeatedBlocks[index] += 1
+			}
+		}
+	}
+
+	// Find any repeated block(s)
+	isECB := false
+	for _, freq := range repeatedBlocks {
+		if freq >= 1 {
+			isECB = true
+		}
+	}
 	
-	return highestCount, result
+	return isECB
 }
--- set1_basics/s1c8_detectAESinECBmode_test.go
+++ set1_basics/s1c8_detectAESinECBmode_test.go
@@ -3,7 +3,7 @@ package set1_basics
 import "testing"
 
 func TestDetectAES128_ECB(t *testing.T) {
-	_, actual := DetectAES128_ECB("../data/s1c8_encodedciphertextstrings.txt")
+	actual := DetectAES128_ECB_File("../data/s1c8_encodedciphertextstrings.txt")
     expected := "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052" +
 	"f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e" +
 	"82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06" +
